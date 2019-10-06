5bb9f13b5197636adbd775f224740e2146509f82
Mark Anderson <mark@chef.io>
2019-10-06 12:55:04 -0700

    WIP Starts a machine
    Signed-off-by: Mark Anderson <mark@chef.io>

---
+++ simple_aws_windows.tf
@@ -0,0 +1,89 @@
+
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "host_role" {
+  type = string
+  default = "HostBucketAccess_Careless"
+}
+
+variable "host_security_group" {
+  type = string
+  default = "launch-wizard-1"
+}
+
+#
+# Note: Set up your .aws/credentials file, and set profile below to match your desired profile (may be default)
+#
+provider "aws" {
+  profile = "Mark_Tools"
+  region = var.region
+}	
+
+
+# resource "aws_iam_role" "pi_role" {
+#   name = "test_role"
+
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+
+#   tags = {
+#     tag-key = "tag-value"
+#   }
+# }
+
+#resource "iam_instance_profile" "pi_instance_profile" {
+#  
+#}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "id_rsa_2016_01_18"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNz1FOktd9ps6bCZWU+tbeIVcFeWfjbW0UfBN596IO/+RULRBi3HSy1BgS9FhGgeHBvCfIbHuyJh5Nuw/hDEM4LzbJ8JOh5Gfevc3YhKkdk/n3rb4Tj8nFK8Q7XIDSGOEPtZKZw/OyqfPmzwsMWbgZH5HRlR/M2ppZn1OeEyBt78Vf4KQxNbYfD/RSz7eQP78+RcFuX6a9wMLxMO83/M05LHGuG145CLtG+jH0fFgwo2fadrMFV3Uda1oIEAml8FVhpNvPSsFxHwJL1sdR4159hb7iw+mPu1HUHXye3VGtaSiHUnv88iP8eisoL62OAOHop+GC7rQ02WmOMHcDSkTfvfRUHk1bskBITo7Mc6ggGxAIsQVI56eT79xjgqCzckfCpNZkVvLeoyPmQVhvkw8tZ0VSX3aP07FyJig8RYjzB+MZ2ij5SZNhEupMn/5XXqFIRcPt6Fb7dyrNcoh6Nnxwo0VmVWoxFG4iEgXv3jHAL4JSv2RDsIxI0qsLMIprmP8= mark@alecto.local"
+}
+
+
+#resource "aws_vpc" "pi_vpc" {
+#  "vpc-0a81384fda4275cd2"
+#  
+#}
+
+resource "aws_instance" "pi_windows" {
+  # note AMI depends on region.
+  # MS Windows Server 2019 Base for us-west-2, ebs, hvm, ENA
+  # ami             = "ami-0e65e5829cd5a9dcd" # us-west-2
+  ami             = "ami-04203dd87d4abd6f6" # us-east-2
+  instance_type   = "t2.large"
+  vpc_security_group_ids = [aws_default_vpc.default.default_security_group_id]
+#  vpc             = "default"
+#  role           = var.host_role
+  key_name        = aws_key_pair.default.key_name
+
+  
+  # ebs_block_device {
+  #   volume_size = 30 # GiB
+  # }
+}
+
+
+
